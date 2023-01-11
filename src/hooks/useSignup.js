78c8ea22f608fa3b193d17377a0f7ed96eec3fb7
danielkuzminski <dankuzmina@gmail.com>
2023-01-11 12:47:34 +0100

    git create signup form, signup hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { auth } from '../firebase/config'
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+
+  const signup = async (email, password, displayName) => {
+    setError(null)
+    setIsPending(true)
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+      setIsPending(false)
+      console.log(res.user)
+
+      await updateProfile(res.user, displayName)
+    }
+    catch (err){
+      console.log(err.message)
+      setError(err.message)
+      setIsPending(false)
+    }
+  }
+  return {error, isPending, signup}
+}
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
+
+// styles
 import './Signup.css'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>
-      <p>signup</p>
-    </div>
+    <form className='signup-form'>
+      <h2>Zarejestruj się</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email"
+          onChange={(e) => {setEmail(e.target.value)}}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <span>hasło:</span>
+        <input 
+          type="password"
+          onChange={(e) => {setPassword(e.target.value)}}
+          value={password}
+          required
+         />
+      </label>
+      <button className='btn-signup'>Wyślij</button>
+    </form>
   )
 }
