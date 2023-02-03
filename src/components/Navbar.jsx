f8e205d4d09ce2313e986d43a1b5063f5cec40e9
danielkuzminski <dankuzmina@gmail.com>
2023-02-03 04:01:07 +0100

    git update navbar display options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,18 +6,30 @@ import { Link } from 'react-router-dom'
 
 //hooks
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Navbar() {
 
   const {logout} = useLogout()
 
+  const {user} = useAuthContext()
+
   return (
     <nav className='navbar'>
       <ul>
         <li className='title'>Filmy</li>
-        <li className='nav-action'><Link to='/login'>zaloguj</Link></li>
-        <li className='nav-action'><Link to='/signup'>zarejestruj</Link></li>
-        <li className='nav-action clickable' onClick={logout} >wyloguj</li>
+        {!user && (
+          <>
+            <li className='nav-action'><Link to='/login'>zaloguj</Link></li>
+            <li className='nav-action'><Link to='/signup'>zarejestruj</Link></li>
+          </>
+        )}
+        {user && (
+          <>
+            <li className='nav-action'>Witaj, {user.displayName} :)</li>
+            <li className='nav-action clickable' onClick={logout} >wyloguj</li>
+          </>
+          )}
       </ul>
     </nav>
   )
