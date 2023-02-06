706d7f290e6729df10c042596c22250cc7c3f62a
danielkuzminski <dankuzmina@gmail.com>
2023-02-06 09:45:24 +0100

    git create rout guarding, user login status check, pages redirects

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 //router
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 //pages components
 import Home from './pages/Home'
@@ -15,20 +15,28 @@ import NotFound from './pages/NotFound'
 //components
 import Navbar from './components/Navbar';
 
+// authentication
+import { useAuthContext } from './hooks/useAuthContext'
+
 function App() {
+
+  const { authIsReady, user } = useAuthContext()
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/movies' element={<Movies />} />
-          <Route path='/movies/:id' element={<Movie />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      { authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={user ? <Home /> : <Navigate to='/login' />} />
+            <Route path='/movies' element={user ? <Movies /> : <Navigate to='/login'/>} />
+            <Route path='/movies/:id' element={user ? <Movie /> : <Navigate to='/login' />} />
+            <Route path='/signup' element={user ? <Navigate to='/' /> : <Signup />} />
+            <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react'
+
+// styles
 import './NotFound.css'
 
+// router
+import { Navigate, useNavigate } from 'react-router-dom'
+
 export default function NotFound() {
+  const navigate = useNavigate()
+
+  // navigating to main page
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/')
+    }, 5000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <div>
-      <p>404</p>
+    <div className='not-found-container'>
+      <h1>404</h1>
+      <p>Strona o podanym adresie nie istnieje. Za 5 sekund nastąpi przekierowanie na stronę główną</p>
     </div>
   )
 }
