2df13fe3ad208b6e086c6ceb31aa73f79a72aa7b
danielkuzminski <dankuzmina@gmail.com>
2023-02-08 22:38:45 +0100

    git create single movie download logic, style home page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,11 +1,35 @@
+import { useState, useEffect } from 'react'
+
+// styles
 import './Movie.css'
 
-import React from 'react'
+// router
+import { useParams } from 'react-router-dom'
+
+// firebase
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase/config'
+
 
 export default function Movie() {
+  const {id} = useParams()
+
+  const [movie, setMovie] = useState(null)
+
+  useEffect(() => {
+    let ref = doc(db, 'movies', id)
+
+    getDoc(ref)
+    .then((snapshot) => {
+      setMovie(snapshot.data())
+    })
+
+  }, [id])
+
+
   return (
     <div>
-      <p>single movie</p>
+      {movie && movie.titlePl}
     </div>
   )
 }
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -4,15 +4,20 @@ import { useCollection } from '../hooks/useCollection'
 // styles
 import './Movies.css'
 
+// router
+import { Link } from 'react-router-dom'
+
 export default function Movies() {
   const {data: movies} = useCollection('movies')
 
   return (
     <div className='movies-container'>
       {movies && movies.map((movie) => (
-        <div key={movie.id}>
-          <h1>{movie.titlePl}</h1>
-        </div>
+        <Link to={`/movies/${movie.id}`} className='movie-card' key={movie.id}>
+          <p className='movie-title-pl'>"{movie.titlePl}"</p>
+          <span className='movie-title-eng'>{movie.titleEng}</span>
+          <img src={movie.cover} alt={movie.titlePl} />
+        </Link>
       ))}
     </div>
   )
