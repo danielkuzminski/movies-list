a90a08fdd15fc57df50c2a144b103055dd9a4e86
danielkuzminski <dankuzmina@gmail.com>
2023-04-07 03:40:46 +0200

    git style search component and search output

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,36 +1,63 @@
+//firebase
 import { useCollection } from "../hooks/useCollection"
+
+//react
 import { useState } from "react"
 
+//styles
+import "./Search.css"
+
+//components
+import Navbar from "../components/Navbar"
+
+//router
+import { Link } from "react-router-dom"
 
 export default function Search() {
-    const [search, setSearch] = useState('')
-    const {data: movies} = useCollection('movies')
-    
-    // console.log(movies)
-    const filteredFilms = (search, movies) => {
-        if(!search){
-            return movies
-        }
-        return movies.filter(film => film.titlePl.toLowerCase().includes(search))
-    }
-    const filteredMovies = filteredFilms(search, movies)
+	const [search, setSearch] = useState("")
+	const { data: movies } = useCollection("movies")
 
-    console.log(filteredMovies)
+	// console.log(movies)
+	const filteredFilms = (search, movies) => {
+		if (!search) {
+			return movies
+		}
+		return movies.filter((film) =>
+			film.titlePl.toLowerCase().includes(search.toLowerCase())
+		)
+	}
+	const filteredMovies = filteredFilms(search, movies)
 
-  return (
-    <div>
-        <h1>wtf</h1>
-        <input 
-            type="text"
-            onChange={(e) => {
-                setSearch(e.target.value.toLowerCase())
-            }}
-            value={search}
-            />
-            {search && movies && filteredMovies.map(movie => (
-                <div key={movie.id}>{movie.titlePl}</div>
-            ))}
-    </div>
+	// console.log(filteredMovies)
 
-  )
+	return (
+		<div className="search">
+			<Navbar />
+			<div className='search-container'>
+				<div className='search-display'>
+					<h1 className='search-title'>Wyszukaj film</h1>
+					<input
+						type='text'
+						className='search-input'
+						onChange={(e) => {
+							setSearch(e.target.value.toLowerCase())
+						}}
+						value={search}
+					/>
+					{search &&
+						movies &&
+						filteredMovies.map((movie) => (
+							<div className='output-display' key={movie.id}>
+								{/* <p className='title-display'>
+								<Link to={`/movies/${movie.id}`}>{movie.titlePl.toLowerCase()}</Link>
+							</p> */}
+								<Link className='search-link' to={`/movies/${movie.id}`}>
+									<p className='title-display'>{movie.titlePl.toLowerCase()}</p>
+								</Link>
+							</div>
+						))}
+				</div>
+			</div>
+		</div>
+	)
 }
