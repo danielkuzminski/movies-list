a3879daf926ad3ddc252f9f9aed1b5ce0b93b9e3
danielkuzminski <dankuzmina@gmail.com>
2023-04-10 18:36:32 +0200

    git finishing touches to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,20 @@ import { useState } from 'react'
 //styles
 import './Navbar.css'
 
+//router
+import { NavLink } from 'react-router-dom'
+
 //hooks
 import { useAuthContext } from '../hooks/useAuthContext'
+import { useLogout } from '../hooks/useLogout'
 
 //components
 import NavbarDropdown from './NavbarDropdown'
 
 
 export default function Navbar() {
+
+  const {logout} = useLogout()
 
   const {user} = useAuthContext()
 
@@ -31,6 +37,20 @@ export default function Navbar() {
               <span className='hamburger-bar'></span>
         </div>
         {show && <NavbarDropdown showElement={showElement} />}
+        <ul className='to-right'>
+          <li>
+            <NavLink to='/'>główna</NavLink>
+          </li>
+          <li>
+            <NavLink to='/search'>szukaj</NavLink>
+          </li>
+          <li>
+            <NavLink to='/add_movie'>dodaj</NavLink>
+          </li>
+          <li>
+            <NavLink onClick={logout}><span className='nav-logout' >wyloguj</span></NavLink>
+          </li>
+        </ul>
     </nav>
   )
 }
